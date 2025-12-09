6fedbd1a0142280b2e96ac077c324a143792f4a5
mia218796-dot <mia218796@gmail.com>
2025-12-09 08:51:01 -0400

    mis primeros archivos

---
+++ javascript.js
@@ -0,0 +1,10 @@
+// Animación suave al mostrar las tarjetas
+document.querySelectorAll('.tarjeta').forEach((card, index) => {
+    card.style.opacity = "0";
+    card.style.transform = "translateY(30px)";
+    setTimeout(() => {
+        card.style.transition = "1s";
+        card.style.opacity = "1";
+        card.style.transform = "translateY(0)";
+    }, 200 * index);
+});
